d9a01ee858a105f0c69e2acdcc077d62a2be32fb
Jay's <dallardj@gmail.com>
2024-11-27 17:35:42 +0100

    remove scoreboard limit

--- src/Models/Statistics/DeliveryStatistic.php
+++ src/Models/Statistics/DeliveryStatistic.php
@@ -3,7 +3,6 @@
 namespace Seat\HermesDj\Industry\Models\Statistics;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\DB;
@@ -50,8 +49,7 @@ class DeliveryStatistic extends Model
         return self::whereNotNull('completed_at')
             ->selectRaw('count(id) as value, user_id')
             ->groupBy('user_id')
-            ->orderBy('value', 'desc')
-            ->limit('10');
+            ->orderBy('value', 'desc');
     }
 
     public static function queryDeliveryMeanCompletionTimeByUser(): Builder
@@ -59,8 +57,7 @@ class DeliveryStatistic extends Model
         return self::select(DB::raw('AVG(TIME_TO_SEC(TIMEDIFF(completed_at, accepted))) AS value, user_id'))
             ->whereNotNull('completed_at')
             ->groupBy('user_id')
-            ->orderBy('value', 'asc')
-            ->limit('10');
+            ->orderBy('value', 'asc');
     }
 
     public static function queryFastestCompletionTimeByUser(): Builder
@@ -68,7 +65,6 @@ class DeliveryStatistic extends Model
         return self::select(DB::raw('MIN(TIME_TO_SEC(TIMEDIFF(completed_at, accepted))) AS value, user_id'))
             ->whereNotNull('completed_at')
             ->groupBy('user_id')
-            ->orderBy('value', 'asc')
-            ->limit('10');
+            ->orderBy('value', 'asc');
     }
 }
