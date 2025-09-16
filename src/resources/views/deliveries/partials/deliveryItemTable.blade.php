f082b4c7782f6eb811f761fdd7ef94cc247512ec
Jay's <5843492+hermesdj@users.noreply.github.com>
2025-09-16 15:37:09 +0200

    do not display delivery items if order no longer exists

--- src/resources/views/deliveries/partials/deliveryItemTable.blade.php
+++ src/resources/views/deliveries/partials/deliveryItemTable.blade.php
@@ -10,59 +10,64 @@
     </thead>
     <tbody>
     @foreach($items as $item)
-        <tr>
-            <td data-sort="{{$item->orderItem->type->typeName}}">
-                @include("web::partials.type",[
-                    'type_id'   => $item->orderItem->type_id,
-                    'type_name' => $item->orderItem->type->typeName,
-                    'variation' => $item->orderItem->type->group->categoryID == 9 ? 'bpc' : 'icon',
-                ])
-            </td>
-            <td class="text-right" data-sort="{{$item->quantity_delivered}}">
-                {{\Seat\HermesDj\Industry\Helpers\OrderHelper::formatNumber($item->quantity_delivered, 0)}}
-            </td>
-            <td class="text-right" data-sort="{{$item->orderItem->unit_price / 100}}">
-                {{\Seat\HermesDj\Industry\Helpers\OrderHelper::formatNumber($item->orderItem->unit_price / 100)}}
-                ISK
-            </td>
-            <td class="text-right" data-sort="{{$item->orderItem->unit_price / 100 * $item->quantity_delivered}}">
-                {{\Seat\HermesDj\Industry\Helpers\OrderHelper::formatNumber($item->orderItem->unit_price / 100 * $item->quantity_delivered)}}
-                ISK
-            </td>
-            <td class="d-flex flex-row text-center align-middle" style="min-width: 120px;">
-                @can("seat-industry.same-user",$delivery->user_id)
-                    <form action="{{ route("seat-industry.setDeliveryItemState", ['delivery' => $delivery->id, 'item' => $item->id]) }}"
-                          method="POST"
-                          style="width: 50%"
-                    >
-                        @csrf
-                        @if($item->completed)
-                            <button type="submit" class="btn btn-warning btn-sm text-nowrap btn-block">
-                                <i class="fas fa-clock"></i>
-                            </button>
-                            <input type="hidden" name="completed" value="0">
-                        @else
-                            <button type="submit" class="btn btn-success btn-sm text-nowrap btn-block">
-                                <i class="fas fa-check"></i>
-                            </button>
-                            <input type="hidden" name="completed" value="1">
-                        @endif
-                    </form>
-
-                    @if(!$item->completed || auth()->user()->can("seat-industry.admin"))
-                        <form action="{{ route("seat-industry.deleteDeliveryItem",['delivery' => $delivery->id, 'item' => $item->id]) }}"
+        @if($item->orderItem)
+            <tr>
+                <td data-sort="{{$item->orderItem->type->typeName}}">
+                    @include("web::partials.type",[
+                        'type_id'   => $item->orderItem->type_id,
+                        'type_name' => $item->orderItem->type->typeName,
+                        'variation' => $item->orderItem->type->group->categoryID == 9 ? 'bpc' : 'icon',
+                    ])
+                </td>
+                <td class="text-right" data-sort="{{$item->quantity_delivered}}">
+                    {{\Seat\HermesDj\Industry\Helpers\OrderHelper::formatNumber($item->quantity_delivered, 0)}}
+                </td>
+                <td class="text-right" data-sort="{{$item->orderItem->unit_price / 100}}">
+                    {{\Seat\HermesDj\Industry\Helpers\OrderHelper::formatNumber($item->orderItem->unit_price / 100)}}
+                    ISK
+                </td>
+                <td class="text-right" data-sort="{{$item->orderItem->unit_price / 100 * $item->quantity_delivered}}">
+                    {{\Seat\HermesDj\Industry\Helpers\OrderHelper::formatNumber($item->orderItem->unit_price / 100 * $item->quantity_delivered)}}
+                    ISK
+                </td>
+                <td class="d-flex flex-row text-center align-middle" style="min-width: 120px;">
+                    @can("seat-industry.same-user",$delivery->user_id)
+                        <form action="{{ route("seat-industry.setDeliveryItemState", ['delivery' => $delivery->id, 'item' => $item->id]) }}"
                               method="POST"
-                              style="width: 50%">
+                              style="width: 50%"
+                        >
                             @csrf
-                            <button type="submit" class="btn btn-danger btn-sm text-nowrap confirmform ml-1 btn-block"
-                                    data-seat-action="{{trans('seat-industry::ai-deliveries.cancel_delivery_action')}}">
-                                <i class="fas fa-times"></i>
-                            </button>
+                            @if($item->completed)
+                                <button type="submit" class="btn btn-warning btn-sm text-nowrap btn-block">
+                                    <i class="fas fa-clock"></i>
+                                </button>
+                                <input type="hidden" name="completed" value="0">
+                            @else
+                                <button type="submit" class="btn btn-success btn-sm text-nowrap btn-block">
+                                    <i class="fas fa-check"></i>
+                                </button>
+                                <input type="hidden" name="completed" value="1">
+                            @endif
                         </form>
-                    @endif
-                @endcan
-            </td>
-        </tr>
+
+                        @if(!$item->completed || auth()->user()->can("seat-industry.admin"))
+                            <form action="{{ route("seat-industry.deleteDeliveryItem",['delivery' => $delivery->id, 'item' => $item->id]) }}"
+                                  method="POST"
+                                  style="width: 50%">
+                                @csrf
+                                <button type="submit"
+                                        class="btn btn-danger btn-sm text-nowrap confirmform ml-1 btn-block"
+                                        data-seat-action="{{trans('seat-industry::ai-deliveries.cancel_delivery_action')}}">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </form>
+                        @endif
+                    @endcan
+                </td>
+            </tr>
+        @else
+            <tr><td>Order No Longer Exists, cannot display items.</td></tr>
+        @endif
     @endforeach
     </tbody>
 </table>
